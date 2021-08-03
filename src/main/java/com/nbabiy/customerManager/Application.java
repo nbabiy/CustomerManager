42da2b10e59ff828c2993bf992f0b34c3ac114d9
Nazarii Babii <n.babiy69@gmail.com>
2021-08-03 19:45:47 +0300

    Add spring boot: starter, data-jpa, web. Define Customer Entity. Add configuration for postgresql

---
+++ src/main/java/com/nbabiy/customerManager/Application.java
@@ -0,0 +1,15 @@
+package com.nbabiy.customerManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author nazariibabii
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
